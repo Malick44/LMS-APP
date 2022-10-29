0aa9505073f06cb4e41a5f6fbaa1fcfaec179beb
Malick44 <100083361+Malick44@users.noreply.github.com>
2022-10-29 14:45:28 -0400

    uodate

--- LMS-APP-Spring Boot/src/main/java/com/controller/CourseController.java
+++ LMS-APP-Spring Boot/src/main/java/com/controller/CourseController.java
@@ -1,4 +1,26 @@
 package com.controller;
 
+import com.model.Course;
+import com.service.CourseService;
+import com.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/course")
 public class CourseController {
+    @Autowired
+    private CourseService courseService;
+
+    public ResponseEntity<Course> getCourse(Integer courseId){
+
+
+        ResponseEntity<Course>  course = courseService.getCourse(Integer courseId);
+
+        return null;
+
+    }
+
 }
